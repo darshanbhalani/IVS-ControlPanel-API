f741d7aa5814a808f279266025a534c80ef95481
DARSHAN BHALANI <darsganbhalanipatel@gmail.com>
2024-07-01 00:03:52 +0530

    done

--- Controllers/Account/AccountController.cs
+++ Controllers/Account/AccountController.cs
@@ -39,7 +39,7 @@ namespace IVS_API.Controllers.Account
                     {
                         if (reader.Read())
                         {
-                            var token = generateToken(userName, reader.GetString(reader.GetOrdinal("employeename")), reader.GetInt32(reader.GetOrdinal("employeedesignation")));
+                            var token = generateToken(reader.GetInt64(reader.GetOrdinal("employeeid")), reader.GetString(reader.GetOrdinal("employeename")), reader.GetInt32(reader.GetOrdinal("employeedesignation")));
                              return Ok(new { success = true, header = new { requestTime = timeStamp, responsTime = TimeZoneIST.now() }, body = new { message = "Login Successfull.", loginToken=token } });
                         }
                         else
@@ -59,15 +59,55 @@ namespace IVS_API.Controllers.Account
             }
         }
 
+        [HttpGet("GetUserProfileDetails")]
+        public IActionResult GetUserProfileDetails(long userId)
+        {
+            DateTime timeStamp = TimeZoneIST.now();
+            UserModel user = new UserModel();
+            try
+            {
+                using (var cmd = new NpgsqlCommand("SELECT * FROM IVS_ACCOUNT_GETUSERPROFILE(@in_userId)", _connection))
+                {
+                    cmd.Parameters.AddWithValue("in_userId", userId);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            user.Code = reader.GetString(reader.GetOrdinal("employeecode"));
+                            user.ProfileUrl = reader.GetString(reader.GetOrdinal("employeeprofileurl"));
+                            user.UserName  = reader.GetString(reader.GetOrdinal("employeename"));
+                            user.Email = reader.GetString(reader.GetOrdinal("employeeemail"));
+                            user.PhoneNumber = reader.GetInt64(reader.GetOrdinal("employeephonenumber"));
+                            user.Gender = reader.GetString(reader.GetOrdinal("employeegender"));
+                            user.Role = reader.GetString(reader.GetOrdinal("employeedesignationname"));
 
-        private string generateToken(string employeeCode,string employeeName,int employeeRoleId)
+                            return Ok(new { success = true, header = new { requestTime = timeStamp, responsTime = TimeZoneIST.now() }, body = new { message = "User profile data successfully fetched.", data = user } });
+                        }
+                        else
+                        {
+                            return Ok(new { success = false, header = new { requestTime = timeStamp, responsTime = TimeZoneIST.now() }, body = new { error = "Unable to fetch user profile data." } });
+                        }
+                    }
+                }
+            }
+            catch (NpgsqlException pex)
+            {
+                return Ok(new { success = false, header = new { requestTime = timeStamp, responsTime = TimeZoneIST.now() }, error = pex.Message });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { success = false, header = new { requestTime = timeStamp, responsTime = TimeZoneIST.now() }, error = ex.Message });
+            }
+        }
+
+        private string generateToken(long employeeId,string employeeName,int employeeRoleId)
         {
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var claims = new[]
             {
-        new Claim("employeeCode", employeeCode), 
+        new Claim("employeeId", employeeId.ToString()), 
         new Claim("employeeName", employeeName), 
         new Claim("tokenType", "employee-login"), 
         new Claim("createdOn", DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffZ")), 
